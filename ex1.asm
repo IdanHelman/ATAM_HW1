e8bfc798a5d1bc36b3acbd4de7307085e054fc53
DavidBuskila <davidbuskila@campus.technion.ac.il>
2024-07-06 22:48:21 +0300

    passing address tests

--- ex1.asm
+++ ex1.asm
@@ -4,19 +4,28 @@
 _start:
 
 movq Adress(%rip), %rbx
-movb $0, Legal
-cmpq $0, %rbx #checks if received address is 0
-je end_HW1 #if so, end the program
+cmpq $0, %rbx
+je end_HW1 #check if array address is 0
 movzx Index(%rip), %rcx
-movb $1, Legal(%rip)
+movb $0, Legal(%rip)
 cmpl length(%rip), %ecx #index-length
 jl IndexInRange_HW1 #if index>=size, go to end of program with legal value=0
-movb $0, Legal(%rip)
+movb $0, Legal
 jmp end_HW1
 
 IndexInRange_HW1:
-movl 0(%rbx, %rcx, 4), %eax
+lea 0(%rcx, %rcx, 2), %rax
+addq %rcx, %rax
+addq %rax, %rbx
+jc end_HW1 # checking if arr[index] address is crossing legal bound of memory
+movzx length(%rip), %r9
+lea 0(%r9, %r9, 2), %rdx
+addq %r9, %rdx
+addq Adress(%rip), %rdx
+jc end_HW1 #check if array is crossing legal bound of memory
+movl (%rbx), %eax
 movl %eax, num(%rip)
+movb $1, Legal(%rip)
 jmp end_HW1
 
 end_HW1:
